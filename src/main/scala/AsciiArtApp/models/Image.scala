8386b0761ffb01a5579329f90e4071795a9ea2c9
Marko Hujo <hujomark@fit.cvut.cz>
2022-10-28 23:37:14 +0200

    Rename class from PixelGrid to Image

--- src/main/scala/AsciiArtApp/models/Image.scala
+++ src/main/scala/AsciiArtApp/models/Image.scala
@@ -2,6 +2,6 @@ package AsciiArtApp.models
 
 import AsciiArtApp.models.pixel.Pixel
 
-class PixelGrid[T <: Pixel](id: Long, pixels: Seq[Seq[T]]) extends Entity[Long] {
+class Image[T <: Pixel](id: Long, pixels: Seq[Seq[T]]) extends Entity[Long] {
   override def getId: Long = id
 }
