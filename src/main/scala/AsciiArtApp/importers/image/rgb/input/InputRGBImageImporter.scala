e293c454d6499f6b111e64be7ee5eae42e164db3
Marko Hujo <hujomark@fit.cvut.cz>
2022-10-31 15:39:55 +0100

    Fix bug with loading image

--- src/main/scala/AsciiArtApp/importers/image/rgb/input/InputRGBImageImporter.scala
+++ src/main/scala/AsciiArtApp/importers/image/rgb/input/InputRGBImageImporter.scala
@@ -8,17 +8,16 @@ import java.awt.Color
 import java.awt.image.BufferedImage
 
 trait InputRGBImageImporter extends RGBImageImporter {
-
-  def createImageFromBufferedImage(image: BufferedImage): Image[RGBPixel] = {
+  protected def createImageFromBufferedImage(image: BufferedImage): Image[RGBPixel] = {
     var pixels: Seq[Seq[RGBPixel]] = Seq.empty[Seq[RGBPixel]]
-    for (i <- 0 until image.getHeight) {
+
+    for (i <- 0 until image.getHeight()) {
       var pixelRow: Seq[RGBPixel] = Seq.empty[RGBPixel]
-      for (j <- 0 until image.getWidth)
-        pixelRow = pixelRow.appended(new RGBPixel(new Color(image.getRGB(i, j))))
-      pixels = pixels.appended(pixelRow)
+      for (j <- 0 until image.getWidth())
+        pixelRow = pixelRow.appended(new RGBPixel(new Color(image.getRGB(j, i))))
+      pixels = pixels.prepended(pixelRow)
     }
 
     new Image[RGBPixel](new PixelGrid[RGBPixel](pixels))
   }
-
 }
--- src/main/scala/AsciiArtApp/importers/image/rgb/random/RandomRGBImageImporter.scala
+++ src/main/scala/AsciiArtApp/importers/image/rgb/random/RandomRGBImageImporter.scala
@@ -5,5 +5,7 @@ import AsciiArtApp.models.Image
 import AsciiArtApp.models.pixel.RGBPixel
 
 class RandomRGBImageImporter extends RGBImageImporter {
-  override def load: Image[RGBPixel] = ???
+  override def load: Image[RGBPixel] = {
+    ???
+  }
 }
