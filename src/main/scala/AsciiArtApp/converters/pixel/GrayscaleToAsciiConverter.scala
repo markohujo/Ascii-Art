6c3e4b94098cbf74c1d661cc082fce7ada448561
Marko Hujo <hujomark@fit.cvut.cz>
2022-10-31 21:48:12 +0100

    Add converter from gs to ascii

--- src/main/scala/AsciiArtApp/converters/pixel/GrayscaleToAsciiConverter.scala
+++ src/main/scala/AsciiArtApp/converters/pixel/GrayscaleToAsciiConverter.scala
@@ -3,7 +3,11 @@ package AsciiArtApp.converters.pixel
 import AsciiArtApp.models.pixel.{AsciiPixel, GrayscalePixel}
 
 class GrayscaleToAsciiConverter extends PixelConverter[GrayscalePixel, AsciiPixel] {
+
+  private val chars: String = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+
   override def convert(from: GrayscalePixel): AsciiPixel = {
-    ???
+    val index: Int = ((chars.length - 1) * from.getValue / 255).toInt
+    new AsciiPixel(chars.charAt(index))
   }
 }
--- src/main/scala/AsciiArtApp/models/pixel/GrayscalePixel.scala
+++ src/main/scala/AsciiArtApp/models/pixel/GrayscalePixel.scala
@@ -1,3 +1,6 @@
 package AsciiArtApp.models.pixel
 
-class GrayscalePixel(value: Double) extends Pixel {}
+class GrayscalePixel(value: Double) extends Pixel {
+  def getValue: Double = value
+
+}
