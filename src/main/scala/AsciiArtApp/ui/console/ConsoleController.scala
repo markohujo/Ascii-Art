f38b244b83f19b062dfdd550c08b8dd34df322e6
Marko Hujo <hujomark@fit.cvut.cz>
2022-11-14 13:14:51 +0100

    Add controller interface

--- src/main/scala/AsciiArtApp/ui/console/ConsoleController.scala
+++ src/main/scala/AsciiArtApp/ui/console/ConsoleController.scala
@@ -5,8 +5,25 @@ import AsciiArtApp.ui.Controller
 class ConsoleController extends Controller[Array[String]] {
 
   override def processUserInput(input: Array[String]): Unit = {
-    println(input.length)
-    input.foreach(println)
+    if (input.isEmpty)
+      throw new IllegalArgumentException("No arguments were given by the user.")
+
+    sortArguments(parseInput(input)).foreach(executeArgument)
+  }
+
+  private def parseInput(input: Array[String]): Seq[String] = {
+    ???
+  }
+
+  private def sortArguments(arguments: Seq[String]): Seq[String] = {
+    ???
+  }
+
+  private def executeArgument(argument: String): Unit = {
+    ???
+
+    // TODO arguments could be case classes ???
+    // TODO pattern matching ???
   }
 
 }
