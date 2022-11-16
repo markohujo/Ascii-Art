8f54157c0e940e212ca0ba946545a943f8e61c65
Marko Hujo <hujomark@fit.cvut.cz>
2022-11-16 15:01:27 +0100

    Add interface for ascii filters

---
+++ src/main/scala/AsciiArtApp/filters/image/ascii/ScaleImageFilter.scala
@@ -0,0 +1,9 @@
+package AsciiArtApp.filters.image.ascii
+import AsciiArtApp.models.Image
+import AsciiArtApp.models.pixel.AsciiPixel
+
+class ScaleImageFilter extends AsciiImageFilter {
+  override def filter(item: Image[AsciiPixel]): Image[AsciiPixel] = {
+    ???
+  }
+}
