bddc0af5968d60a011e6063c9a45c98257d53813
Marko Hujo <hujomark@fit.cvut.cz>
2022-12-05 23:31:39 +0100

    Add image tests

--- src/test/scala/app/models/ImageTest.scala
+++ src/test/scala/app/models/ImageTest.scala
@@ -1,7 +1,23 @@
 package app.models
 
+import app.models.pixel.RGBPixel
+import org.mockito.ArgumentMatchers.{any, anyInt}
+import org.mockito.Mockito.verify
+import org.mockito.MockitoSugar.mock
 import org.scalatest.FunSuite
 
 class ImageTest extends FunSuite {
 
+  private val pixelGridMock = mock[PixelGrid[RGBPixel]]
+  private val image: Image[RGBPixel] = new Image[RGBPixel](pixelGridMock)
+
+  test("transform test should call pixel grid") {
+    image.pixelAt(0, 0)
+    verify(pixelGridMock).at(anyInt(), anyInt())
+  }
+
+  test("pixel at test should call pixel grid") {
+    image.transform(any())
+    verify(pixelGridMock).transform(any())
+  }
 }
