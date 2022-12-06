12587838688d3776657b8357f0f2458c02c6f922
Marko Hujo <hujomark@fit.cvut.cz>
2022-12-06 15:06:37 +0100

    Add image facade tests

--- src/test/scala/app/facades/ImageFacadeTest.scala
+++ src/test/scala/app/facades/ImageFacadeTest.scala
@@ -1,8 +1,12 @@
 package app.facades
 
 
+import app.models.Image
+import app.models.pixel.{AsciiPixel, GreyscalePixel, RGBPixel}
+import filters.image.greyscale.BrightnessImageFilter
 import importers.image.rgb.RGBImageImporter
-import org.mockito.Mockito.verify
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{verify, when}
 import org.mockito.MockitoSugar.mock
 import org.scalatest.FunSuite
 
@@ -29,7 +33,20 @@ class ImageFacadeTest extends FunSuite {
   }
 
   test("translate image test") {
+    val importerMock = mock[RGBImageImporter]
+    val imageMock = mock[Image[RGBPixel]]
+    val greyscaleImageMock = mock[Image[GreyscalePixel]]
+    val asciiImageMock = mock[Image[AsciiPixel]]
 
+    when(importerMock.load).thenReturn(imageMock)
+    when(imageMock.transform(any[RGBPixel => GreyscalePixel])).thenReturn(greyscaleImageMock)
+    when(greyscaleImageMock.transform(any[GreyscalePixel => AsciiPixel])).thenReturn(asciiImageMock)
+
+    facade.loadImage(importerMock)
+    facade.translateImage()
+
+    verify(importerMock).load
+    verify(imageMock).transform(any[RGBPixel => GreyscalePixel])
+    verify(greyscaleImageMock).transform(any[GreyscalePixel => AsciiPixel])
   }
-
 }
