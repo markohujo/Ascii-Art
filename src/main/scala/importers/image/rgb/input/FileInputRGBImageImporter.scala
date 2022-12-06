882fe3c5dd2da7fae724afbd057710aca2f3cddc
Marko Hujo <hujomark@fit.cvut.cz>
2022-12-06 15:07:03 +0100

    Fix load method in FileInputRGBImageImporter

--- src/main/scala/importers/image/rgb/input/FileInputRGBImageImporter.scala
+++ src/main/scala/importers/image/rgb/input/FileInputRGBImageImporter.scala
@@ -12,6 +12,9 @@ class FileInputRGBImageImporter(path: String) extends InputRGBImageImporter {
     throw new IllegalArgumentException("Unsupported file extension.")
 
   override def load: Image[RGBPixel] = {
-    createImageFromBufferedImage(ImageIO.read(new File(path)))
+    val file = new File(path)
+    if (!file.canRead)
+      throw new IllegalArgumentException("Cannot open file with the given path")
+    createImageFromBufferedImage(ImageIO.read(file))
   }
 }
